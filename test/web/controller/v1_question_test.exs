6b908b4e8aebab7335a254046c38ca3d3260d92e
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2018-01-09 15:26:32 +0900

    [131661] Fix description in v1_question_test.exs

--- test/web/controller/v1_question_test.exs
+++ test/web/controller/v1_question_test.exs
@@ -3,14 +3,14 @@ defmodule MasakiStackoverflow.Controller.V1.QuestionTest do
 
   @create_input %{"title" => "title987", "author" => "author765", "body" => "body432"}
 
-  test "create should fail if invalid values were sent" do
+  test "create should return 403, if invalid values were sent" do
     :meck.expect(Sazabi.G2gClient, :send, fn _, _, _ -> assert false end)
     assert Req.post_json("/v1/question", %{"title" => "",      "author" => "author", "body" => "body"}).status == 403
     assert Req.post_json("/v1/question", %{"title" => "title", "author" => "",       "body" => "body"}).status == 403
     assert Req.post_json("/v1/question", %{"title" => "title", "author" => "author", "body" => ""    }).status == 403
   end
 
-  test "create should succeed if valid values were sent" do
+  test "create should return 201, if valid values were sent" do
     :meck.expect(Sazabi.G2gClient, :send, fn _conn, _app_id, request ->
       assert %Dodai.CreateDedicatedDataEntityRequest{body: %Dodai.CreateDedicatedDataEntityRequestBody{}} = request
       Dodai.CreateDedicatedDataEntitySuccess.new(201, [], "")
