65fa49d4a3defa1548f164a706083201e983059e
aNobuhisaMasaki <nobuhisa.masaki@access-company.com>
2018-01-09 15:27:25 +0900

    [131661] Fix question list order

--- web/controller/question.ex
+++ web/controller/question.ex
@@ -6,7 +6,7 @@ defmodule MasakiStackoverflow.Controller.Question do
   def index(%Conn{context: context} = conn) do
     %{"app_id" => app_id, "group_id" => group_id, "root_key" => root_key} = MasakiStackoverflow.get_all_env()
     # TODO: Pagination into 1000 documents is needed.
-    query   = %Dodai.RetrieveDedicatedDataEntityListRequestQuery{query: %{}, sort: %{"updatedAt": -1}}
+    query   = %Dodai.RetrieveDedicatedDataEntityListRequestQuery{query: %{}, sort: %{"createdAt": -1}}
     request = Dodai.RetrieveDedicatedDataEntityListRequest.new(group_id, @collection_name, root_key, query)
     %Dodai.RetrieveDedicatedDataEntityListSuccess{body: body} = Sazabi.G2gClient.send(context, app_id, request)
     render(conn, 200, "question", [questions: body])
