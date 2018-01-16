4c8b098541980e3f21541facd30a7ee8aa1454c5
nobuhisa.masaki@access-company.com <nobuhisa.masaki@access-company.com>
2018-01-16 15:44:25 +0900

    [131661] RESTify frontend: url as a query

--- web/router.ex
+++ web/router.ex
@@ -2,9 +2,22 @@ defmodule MasakiStackoverflow.Router do
   use SolomonLib.Router
 
   static_prefix "/priv/static"
-  get    "/question",        Question,    :index
-  get    "/question/:id",    Question,    :show
-  post   "/v1/question",     V1.Question, :create
-  put    "/v1/question/:id", V1.Question, :update
-  delete "/v1/question/:id", V1.Question, :delete
+  get    "/question",              Question, :index
+  get    "/question/:question_id", Question, :show
+
+  post   "/v1/question",              V1.Question, :create
+  put    "/v1/question/:question_id", V1.Question, :update
+  delete "/v1/question/:question_id", V1.Question, :delete
+
+  post   "/v1/question/:question_id/comment",             V1.Question, :update
+  put    "/v1/question/:question_id/comment/:comment_id", V1.Question, :update
+  delete "/v1/question/:question_id/comment/:comment_id", V1.Question, :update
+
+  post   "/v1/question/:question_id/answer",              V1.Question, :update
+  put    "/v1/question/:question_id/answer/:answer_id",   V1.Question, :update
+  delete "/v1/question/:question_id/answer/:answer_id",   V1.Question, :update
+
+  post   "/v1/question/:question_id/answer/:answer_id/comment",             V1.Question, :update
+  put    "/v1/question/:question_id/answer/:answer_id/comment/:comment_id", V1.Question, :update
+  delete "/v1/question/:question_id/answer/:answer_id/comment/:comment_id", V1.Question, :update
 end
